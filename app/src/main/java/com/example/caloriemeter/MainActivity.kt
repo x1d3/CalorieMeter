f72deec8e81234e1aa9f0de1da25418278a1f09a
x1d3 <dimasik7557@gmail.com>
2024-06-03 03:27:21 +0300

    main activity functionality

--- app/src/main/java/com/example/caloriemeter/MainActivity.kt
+++ app/src/main/java/com/example/caloriemeter/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.caloriemeter
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.caloriemeter.caloriesCalculator
+import com.example.caloriemeter.calorieCounterRequired
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Находим кнопку "start" и устанавливаем обработчик нажатия
+        findViewById<View>(R.id.start).setOnClickListener {
+            val intent = Intent(this, caloriesCalculator::class.java)
+            startActivity(intent)
+        }
+
+        // Находим кнопку "caloriecounter" и устанавливаем обработчик нажатия
+        findViewById<View>(R.id.caloriecounter).setOnClickListener {
+            val intent = Intent(this, calorieCounterRequired::class.java)
+            startActivity(intent)
+        }
     }
 }
