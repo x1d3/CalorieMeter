f36ed2de048faf19d21f1288d7df618422b67e77
x1d3 <dimasik7557@gmail.com>
2024-06-03 03:04:47 +0300

    added some activities

---
+++ app/src/main/java/com/example/caloriemeter/caloriesCalculator.kt
@@ -0,0 +1,11 @@
+package com.example.caloriemeter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class caloriesCalculator : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calories_calculator)
+    }
+}
